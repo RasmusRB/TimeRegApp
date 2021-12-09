38eee5a0cfce86b578a476aba55e1d9fe6026cbc
Nikolai Køhler Lundberg Hansen <niko9946@edu.easj.dk>
2021-12-09 11:12:56 +0100

    TimeRegistration page

---
+++ src/pages/TimeRegistration.tsx
@@ -0,0 +1,71 @@
+import { useHistory } from "react-router";
+import React, { useState, useEffect } from "react";
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonLabel,
+  IonMenuButton,
+  IonMenuToggle,
+  IonPage,
+  IonRippleEffect,
+  IonTextarea,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+
+const TimeRegistration: React.FC = () => {
+
+  function showDate(){
+    var today = new Date();
+    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+    //var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    return date;
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+            <IonMenuButton />
+          <IonTitle style={{ textAlign: "center" }}>Time Registration</IonTitle>
+          
+          <p style={{ textAlign: "center" }}> {showDate()} </p>
+          
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen >
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <form>
+          <IonInput placeholder="Start time" style={{ textAlign: "center" }}></IonInput>
+          <IonInput placeholder="End time" style={{ textAlign: "center" }}></IonInput>
+
+          <div style={{ textAlign: "center", justifyContent: "center", display: "grid", alignContent: "center" }}>
+            <select name="noget" id="noget" >
+              <option value="noget" selected>Choose activity</option>
+              <option value="noget">Programming</option>
+              <option value="noget">Meeting</option>
+              <option value="noget">Planning</option>
+            </select>
+          </div>
+
+          <IonTextarea placeholder="Comments" style={{ textAlign: "center" }}></IonTextarea>
+
+          <div style={{ textAlign: "center", justifyContent: "center", display: "grid", alignContent: "center" }}>
+            <IonButton>Submit</IonButton>
+          </div>
+
+        </form>
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default TimeRegistration;
